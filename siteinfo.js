630b52db6cdb26177ae1c023430ffdce82d000be
zhrrobert <zhrrobert@hotmail.com>
2019-10-22 04:02:20 +0800

    Signed-off-by: zhrrobert <zhrrobert@hotmail.com>

--- siteinfo.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "云硕 社区团购",
-    uniacid: "26",
-    acid: "26",
-    multiid: "0",
-    version: "7.8.0",
-    siteroot: "https://w.4tiaomao.com/app/index.php",
-    design_method: "3"
-};
