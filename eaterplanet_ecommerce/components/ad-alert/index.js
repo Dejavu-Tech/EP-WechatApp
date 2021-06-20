1478666f8d8e186d192ee4e70416e81669e859ae
zhrrobert <zhrrobert@hotmail.com>
2021-06-20 13:41:41 +0800

    Update 弹窗广告

--- eaterplanet_ecommerce/components/ad-alert/index.js
+++ eaterplanet_ecommerce/components/ad-alert/index.js
@@ -12,7 +12,7 @@ Component({
   },
 
   data: {
-    imgheights: [],//所有图片的高度  
+    imgheights: [],//所有图片的高度
     current:0,
     visible: false,
     adv_list: []
@@ -47,14 +47,14 @@ Component({
       //当图片载入完毕时
       var imgwidth = e.detail.width,
         imgheight = e.detail.height,
-        //宽高比  
+        //宽高比
         ratio = imgwidth / imgheight;
         console.log(imgwidth, imgheight)
       // 计算的高度值
       var viewHeight = 600 / ratio;
       var imgheight = viewHeight;
       var imgheights = this.data.imgheights;
-      //把每一张图片的对应的高度记录到数组里  
+      //把每一张图片的对应的高度记录到数组里
       imgheights[e.target.dataset.id] = imgheight;
       console.log('图片的高度：'+imgheights)
       this.setData({
