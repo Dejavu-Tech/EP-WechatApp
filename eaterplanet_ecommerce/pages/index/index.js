cfb57269e682196af85c0114e5f969454b2bceb9
zhrrobert <zhrrobert@hotmail.com>
2022-06-24 23:56:17 +0800

    Update index.js

--- eaterplanet_ecommerce/pages/index/index.js
+++ eaterplanet_ecommerce/pages/index/index.js
@@ -163,193 +163,6 @@ isIPhoneX: globalData.isIPhoneX,
   postion: {},
   options: '',
   focusFlag: false,
-  /**goweather: function (options) {
-         wx.navigateTo({
-        url: '../../pages/weather/index', 
-        })
-    },
-
-    success (data, location) {
-    this.setData({
-      openSettingButtonShow: false,
-      searchCity: location,
-    })
-    wx.stopPullDownRefresh()
-    let now = new Date()
-    // 存下来源数据
-    data.updateTime = now.getTime()
-    data.updateTimeFormat = utils.formatDate(now, "MM-dd hh:mm")
-    wx.setStorage({
-      key: 'cityDatas',
-      data,
-    })
-    this.setData({
-      cityDatas: data,
-    })
-  },
-  fail(res) {
-    wx.stopPullDownRefresh()
-    let errMsg = res.errMsg || ''
-    // 拒绝授权地理位置权限
-    if (errMsg.indexOf('deny') !== -1 || errMsg.indexOf('denied') !== -1) {
-      wx.showToast({
-        title: '需要开启地理位置权限',
-        icon: 'none',
-        duration: 2500,
-        success: (res) => {
-          if (this.canUseOpenSettingApi()) {
-            let timer = setTimeout(() => {
-              clearTimeout(timer)
-              wx.openSetting({})
-            }, 2500)
-          } else {
-            this.setData({
-              openSettingButtonShow: true,
-            })
-          }
-        },
-      })
-    } else {
-      wx.showToast({
-        title: '网络不给力，请稍后再试',
-        icon: 'none',
-      })
-    }
-  },
-
-  clearInput () {
-    this.setData({
-      searchText: '',
-    })
-  },
-
-
-  canUseOpenSettingApi () {
-    let systeminfo = getApp().globalData.systeminfo
-    let SDKVersion = systeminfo.SDKVersion
-    let version = utils.cmpVersion(SDKVersion, '2.0.7')
-    if (version < 0) {
-      return true
-    } else {
-      return false
-    }
-  },
-  init(params, callback) {
-    this.setData({
-      located: true,
-    })
-    wx.getLocation({
-      success: (res) => {
-        this.getWeather(`${res.latitude},${res.longitude}`)
-        this.getHourly(`${res.latitude},${res.longitude}`)
-        callback && callback()
-      },
-      fail: (res) => {
-        this.fail(res)
-      }
-    })
-  },
-  getWeather (location) {
-    wx.request({
-      url: `${globalData.requestUrl.weather}`,
-      data: {
-        location,
-        key,
-      },
-      success: (res) => {
-        if (res.statusCode === 200) {
-          let data = res.data.HeWeather6[0]
-          if (data.status === 'ok') {
-            this.clearInput()
-            this.success(data, location)
-          } else {
-            wx.showToast({
-              title: '查询失败',
-              icon: 'none',
-            })
-          }
-        }
-      },
-      fail: () => {
-        wx.showToast({
-          title: '查询失败',
-          icon: 'none',
-        })
-      },
-    })
-  },
-  getHourly(location) {
-    wx.request({
-      url: `${globalData.requestUrl.hourly}`,
-      data: {
-        location,
-        key,
-      },
-      success: (res) => {
-        if (res.statusCode === 200) {
-          let data = res.data.HeWeather6[0]
-          if (data.status === 'ok') {
-            this.setData({
-              hourlyDatas: data.hourly || []
-            })
-          }
-        }
-      },
-      fail: () => {
-        wx.showToast({
-          title: '查询失败',
-          icon: 'none',
-        })
-      },
-    })
-  },
-  initSetting (successFunc) {
-    wx.getStorage({
-      key: 'setting',
-      success: (res) => {
-        let setting = res.data || {}
-        this.setData({
-          setting,
-        })
-        successFunc && successFunc(setting)
-      },
-      fail: () => {
-        this.setData({
-          setting: {},
-        })
-      },
-    })
-  },
-  reloadInitSetting () {
-    this.initSetting((setting) => {
-      this.checkUpdate(setting)
-    })
-  },
-  onPullDownRefresh (res) {
-    this.reloadPage()
-  },
-  getCityDatas() {
-    let cityDatas = wx.getStorage({
-      key: 'cityDatas',
-      success: (res) => {
-        this.setData({
-          cityDatas: res.data,
-        })
-      },
-    })
-  },
-  reloadWeather () {
-    if (this.data.located) {
-      this.init({})
-    } else {
-      this.search(this.data.searchCity)
-      this.setData({
-        searchCity: '',
-      })
-    }
-  },*/
-
-
 
   /**
    * 监控滚动事件
@@ -419,7 +232,7 @@ isIPhoneX: globalData.isIPhoneX,
             bh: 'n'
       })
     }
-    /**this.reloadPage()*/
+
     status.setNavBgColor();
     status.setGroupInfo().then((groupInfo) => { that.setData({ groupInfo }) });
     console.log('step1');
@@ -1725,7 +1538,6 @@ isIPhoneX: globalData.isIPhoneX,
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    this.reloadPage()
     this.setData({ couponRefresh: false, newComerRefresh: false, stopNotify: true });
     this.tpage = 1;
     this.$data = {
