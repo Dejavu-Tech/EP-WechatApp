b336dd0d330d53870e8c3a350a6bf921fbd52521
Albert.Z <zhrrobert@hotmail.com>
2024-10-05 22:57:35 +0800

    Update mixins.js

--- eaterplanet_ecommerce/utils/mixins.js
+++ eaterplanet_ecommerce/utils/mixins.js
@@ -1,54 +1,48 @@
 // 保存原生的 Page 函数
-const originPage = Page
+const originPage = Page;
 
 Page = (options) => {
-  const mixins = options.mixins
+  const { mixins } = options;
   // mixins 必须为数组
   if (Array.isArray(mixins)) {
-    delete options.mixins
+    delete options.mixins;
     // mixins 注入并执行相应逻辑
-    options = merge(mixins, options)
+    options = merge(mixins, options);
   }
   // 释放原生 Page 函数
-  originPage(options)
-}
+  originPage(options);
+};
 
 // 定义小程序内置的属性/方法
-const originProperties = ['data', 'properties', 'options']
-const originMethods = ['onLoad', 'onReady', 'onShow', 'onHide', 'onUnload', 'onPullDownRefresh', 'onReachBottom', 'onShareAppMessage', 'onPageScroll', 'onTabItemTap']
+const originProperties = ['data', 'properties', 'options'];
+const originMethods = [
+  'onLoad', 'onReady', 'onShow', 'onHide', 'onUnload',
+  'onPullDownRefresh', 'onReachBottom', 'onShareAppMessage',
+  'onPageScroll', 'onTabItemTap'
+];
 
-function merge(mixins, options) {
-  if (!Object.entries) {
-    Object.entries = function (obj) {
-      var ownProps = Object.keys(obj),
-        i = ownProps.length,
-        resArray = new Array(i); // preallocate the Array
-      while (i--)
-        resArray[i] = [ownProps[i], obj[ownProps[i]]];
-      return resArray;
-    };
-  }
+const merge = (mixins, options) => {
   mixins.forEach((mixin) => {
     if (Object.prototype.toString.call(mixin) !== '[object Object]') {
-      throw new Error('mixin 类型必须为对象！')
+      throw new Error('mixin 类型必须为对象！');
     }
     // 遍历 mixin 里面的所有属性
-    for (let [key, value] of Object.entries(mixin)) {
+    Object.entries(mixin).forEach(([key, value]) => {
       if (originProperties.includes(key)) {
         // 内置对象属性混入
-        options[key] = { ...value, ...options[key] }
+        options[key] = { ...value, ...options[key] };
       } else if (originMethods.includes(key)) {
         // 内置方法属性混入，优先执行混入的部分
-        const originFunc = options[key]
+        const originFunc = options[key];
         options[key] = function (...args) {
-          value.call(this, ...args)
-          return originFunc && originFunc.call(this, ...args)
-        }
+          value.call(this, ...args);
+          return originFunc && originFunc.call(this, ...args);
+        };
       } else {
         // 自定义方法混入
-        options = { ...mixin, ...options }
+        options = { ...mixin, ...options };
       }
-    }
-  })
-  return options
-}
+    });
+  });
+  return options;
+};
