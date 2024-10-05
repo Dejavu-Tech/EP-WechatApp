9774fd66fa60769e6c3a64c1cf71704e27f763cf
Albert.Z <zhrrobert@hotmail.com>
2024-10-05 22:57:39 +0800

    Update timeFormat.js

--- eaterplanet_ecommerce/utils/timeFormat.js
+++ eaterplanet_ecommerce/utils/timeFormat.js
@@ -1,85 +1,96 @@
 Object.defineProperty(exports, "__esModule", {
-  value: !0
+  value: true
 });
 
-var e = exports.formatTime = function(e) {
-    return {
-      year: e.getFullYear(),
-      month: e.getMonth() + 1,
-      day: e.getDate(),
-      hour: e.getHours(),
-      minute: e.getMinutes(),
-      second: e.getSeconds(),
-      weekday: e.getDay()
-    };
-  },
-  t = (exports.formatYMDLocal = function(r) {
-    var n = e(r);
-    return [n.year, n.month, n.day].map(t).join("/");
-  }, exports.formatYMD = function(r) {
-    var n = e(r);
-    return [n.year, n.month, n.day].map(t).join("-");
-  }, exports.formatHMS = function(r) {
-    var n = e(r);
-    return [n.hour, n.minute, n.second].map(t).join(":");
-  }, exports.formatWithoutSecond = function(r) {
-    var n = e(r);
-    return [n.year, n.month, n.day].map(t).join("-") + " " + [n.hour, n.minute].map(t).join(":");
-  }, exports.formatFull = function(r) {
-    var n = e(r);
-    return [n.year, n.month, n.day].map(t).join("-") + " " + [n.hour, n.minute, n.second].map(t).join(":");
-  }, exports.formatMD = function(r) {
-    var n = e(r);
-    return [n.month].map(t) + "月" + [n.day].map(t) + "日";
-  }, exports.formatYMDPoint = function(r) {
-    var n = e(r);
-    return [n.year, n.month, n.day].map(t).join(".");
-  }, exports.formatYMDHMPoint = function(r) {
-    var n = e(r);
-    return [n.year, n.month, n.day].map(t).join(".") + " " + [n.hour, n.minute].map(t).join(":");
-  }, exports.formatMDHM = function(r) {
-    var n = e(r);
-    return [n.month].map(t) + "." + [n.day].map(t) + " " + [n.hour, n.minute].map(t).join(":");
-  }, exports.formatWeekday = function(t) {
-    var r = e(t),
-      n = "";
-    switch (r.weekday) {
-      case 0:
-        n = "周日";
-        break;
+const formatTime = (date) => ({
+  year: date.getFullYear(),
+  month: date.getMonth() + 1,
+  day: date.getDate(),
+  hour: date.getHours(),
+  minute: date.getMinutes(),
+  second: date.getSeconds(),
+  weekday: date.getDay()
+});
 
-      case 1:
-        n = "周一";
-        break;
+const formatNumber = (n) => n.toString().padStart(2, '0');
 
-      case 2:
-        n = "周二";
-        break;
-
-      case 3:
-        n = "周三";
-        break;
-
-      case 4:
-        n = "周四";
-        break;
-
-      case 5:
-        n = "周五";
-        break;
-
-      case 6:
-        n = "周六";
-    }
-    return r.weekday = n, r;
-  }, exports.formatNumber = function(e) {
-    return (e = e.toString())[1] ? e : "0" + e;
-  });
-
-exports.getBeginTime = function(r, n) {
-  var o = new Date(new Date(1 * n).getTime() + 864e5);
-  o.setHours(0), o.setMinutes(0), o.setSeconds(0);
-  var a = o.getTime(),
-    u = e(new Date(1 * r));
-  return a > r ? "今日" + [u.hour, u.minute].map(t).join(":") + "开抢" : a < r && a + 864e5 > r ? "明日" + [u.hour, u.minute].map(t).join(":") + "开抢" : "即将开抢";
+const formatYMDLocal = (date) => {
+  const { year, month, day } = formatTime(date);
+  return `${year}/${formatNumber(month)}/${formatNumber(day)}`;
 };
+
+const formatYMD = (date) => {
+  const { year, month, day } = formatTime(date);
+  return `${year}-${formatNumber(month)}-${formatNumber(day)}`;
+};
+
+const formatHMS = (date) => {
+  const { hour, minute, second } = formatTime(date);
+  return `${formatNumber(hour)}:${formatNumber(minute)}:${formatNumber(second)}`;
+};
+
+const formatWithoutSecond = (date) => {
+  const { year, month, day, hour, minute } = formatTime(date);
+  return `${year}-${formatNumber(month)}-${formatNumber(day)} ${formatNumber(hour)}:${formatNumber(minute)}`;
+};
+
+const formatFull = (date) => {
+  const { year, month, day, hour, minute, second } = formatTime(date);
+  return `${year}-${formatNumber(month)}-${formatNumber(day)} ${formatNumber(hour)}:${formatNumber(minute)}:${formatNumber(second)}`;
+};
+
+const formatMD = (date) => {
+  const { month, day } = formatTime(date);
+  return `${formatNumber(month)}月${formatNumber(day)}日`;
+};
+
+const formatYMDPoint = (date) => {
+  const { year, month, day } = formatTime(date);
+  return `${year}.${formatNumber(month)}.${formatNumber(day)}`;
+};
+
+const formatYMDHMPoint = (date) => {
+  const { year, month, day, hour, minute } = formatTime(date);
+  return `${year}.${formatNumber(month)}.${formatNumber(day)} ${formatNumber(hour)}:${formatNumber(minute)}`;
+};
+
+const formatMDHM = (date) => {
+  const { month, day, hour, minute } = formatTime(date);
+  return `${formatNumber(month)}.${formatNumber(day)} ${formatNumber(hour)}:${formatNumber(minute)}`;
+};
+
+const formatWeekday = (date) => {
+  const weekdays = ["周日", "周一", "周二", "周三", "周四", "周五", "周六"];
+  const { weekday } = formatTime(date);
+  return weekdays[weekday];
+};
+
+const getBeginTime = (startTime, endTime) => {
+  const nextDay = new Date(new Date(endTime).getTime() + 86400000);
+  nextDay.setHours(0, 0, 0, 0);
+  const nextDayTime = nextDay.getTime();
+  const { hour, minute } = formatTime(new Date(startTime));
+  const formattedTime = `${formatNumber(hour)}:${formatNumber(minute)}`;
+
+  if (nextDayTime > startTime) {
+    return `今日${formattedTime}开抢`;
+  } else if (nextDayTime < startTime && nextDayTime + 86400000 > startTime) {
+    return `明日${formattedTime}开抢`;
+  } else {
+    return "即将开抢";
+  }
+};
+
+exports.formatTime = formatTime;
+exports.formatYMDLocal = formatYMDLocal;
+exports.formatYMD = formatYMD;
+exports.formatHMS = formatHMS;
+exports.formatWithoutSecond = formatWithoutSecond;
+exports.formatFull = formatFull;
+exports.formatMD = formatMD;
+exports.formatYMDPoint = formatYMDPoint;
+exports.formatYMDHMPoint = formatYMDHMPoint;
+exports.formatMDHM = formatMDHM;
+exports.formatWeekday = formatWeekday;
+exports.getBeginTime = getBeginTime;
+exports.formatNumber = formatNumber;
