b9639d8944610fa05f84c4b0a6879560f9622cc8
Albert.Z <zhrrobert@hotmail.com>
2024-10-05 22:57:45 +0800

    Update timeQueue.js

--- eaterplanet_ecommerce/utils/timeQueue.js
+++ eaterplanet_ecommerce/utils/timeQueue.js
@@ -4,40 +4,43 @@ exports.default = class {
     this.timer = -1;
   }
 
-  action(){
-    if ("{}" !== JSON.stringify(this.queue)) {
-      for (let i in this.queue) this.queue[i][0]();
-      this.timer = -1, this.begin();
+  action = () => {
+    if (Object.keys(this.queue).length) {
+      Object.values(this.queue).forEach(([fn]) => fn());
+      this.timer = -1;
+      this.begin();
     } else {
       this.stop();
     }
   }
 
-  add(e){
-    var t = "" + new Date().getTime() + Math.ceil(1000 * Math.random());
-    return this.queue["" + t] = [e], -1 === this.timer && this.start(), t;
+  add = (e) => {
+    const t = `${Date.now()}${Math.ceil(1000 * Math.random())}`;
+    this.queue[t] = [e];
+    if (this.timer === -1) this.start();
+    return t;
   }
 
-  remove(e) {
-    delete this.queue["" + e], "{}" === JSON.stringify(this.queue) && (this.timer = -1);
+  remove = (e) => {
+    delete this.queue[e];
+    if (!Object.keys(this.queue).length) this.timer = -1;
   }
 
-  del() {
-    this.queue = {}, "{}" === JSON.stringify(this.queue) && (this.timer = -1);
+  del = () => {
+    this.queue = {};
+    if (!Object.keys(this.queue).length) this.timer = -1;
   }
 
-  stop() {
-    clearTimeout(this.timer), this.timer = -1;
+  stop = () => {
+    clearTimeout(this.timer);
+    this.timer = -1;
   }
 
-  start() {
-    this.timer > -1 || this.action();
+  start = () => {
+    if (this.timer === -1) this.action();
   }
 
-  begin() {
-    var that = this;
-    this.timer = setTimeout(function () {
-      that.action();
-    }, 1000);
+  begin = () => {
+    this.timer = setTimeout(this.action, 1000);
   }
 }
