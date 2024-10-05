4c75c4aeeb2fc9b56e442d4f3f3f0fe538d87c3f
Albert.Z <zhrrobert@hotmail.com>
2024-10-05 22:57:50 +0800

    Update WxValidate.js

--- eaterplanet_ecommerce/utils/WxValidate.js
+++ eaterplanet_ecommerce/utils/WxValidate.js
@@ -1,10 +1,3 @@
-/**
- * 表单验证
- * 
- * @param {Object} rules 验证字段的规则
- * @param {Object} messages 验证字段的提示信息
- * 
- */
 class WxValidate {
     constructor(rules = {}, messages = {}) {
         Object.assign(this, {
@@ -15,26 +8,20 @@ class WxValidate {
         this.__init()
     }
 
-    /**
-     * __init
-     */
+    // __init
     __init() {
         this.__initMethods()
         this.__initDefaults()
         this.__initData()
     }
 
-    /**
-     * 初始化数据
-     */
+    // 初始化数据
     __initData() {
         this.form = {}
         this.errorList = []
     }
 
-    /**
-     * 初始化默认提示信息
-     */
+    // 初始化默认提示信息
     __initDefaults() {
         this.defaults = {
             messages: {
@@ -59,15 +46,11 @@ class WxValidate {
         }
     }
 
-    /**
-     * 初始化默认验证方法
-     */
+    // 初始化默认验证方法
     __initMethods() {
         const that = this
         that.methods = {
-            /**
-             * 验证必填元素
-             */
+            // 验证必填元素
             required(value, param) {
                 if (!that.depend(param)) {
                     return 'dependency-mismatch'
@@ -79,119 +62,80 @@ class WxValidate {
 
                 return value.length > 0
             },
-            /**
-             * 验证电子邮箱格式
-             */
+            // 验证电子邮箱格式
             email(value) {
                 return that.optional(value) || /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(value)
             },
-            /**
-             * 验证手机格式
-             */
+            // 验证手机格式
             tel(value) {
                 return that.optional(value) || /^1[23456789]\d{9}$/.test(value)
             },
-            /**
-             * 验证URL格式
-             */
+            // 验证URL格式
             url(value) {
                 return that.optional(value) || /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value)
             },
-            /**
-             * 验证日期格式
-             */
+            // 验证日期格式
             date(value) {
                 return that.optional(value) || !/Invalid|NaN/.test(new Date(value).toString())
             },
-            /**
-             * 验证ISO类型的日期格式
-             */
+            // 验证ISO类型的日期格式
             dateISO(value) {
                 return that.optional(value) || /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/.test(value)
             },
-            /**
-             * 验证十进制数字
-             */
+            // 验证十进制数字
             number(value) {
                 return that.optional(value) || /^(?:-?\d+|-?\d{1,3}(?:,\d{3})+)?(?:\.\d+)?$/.test(value)
             },
-            /**
-             * 验证整数
-             */
+            // 验证整数
             digits(value) {
                 return that.optional(value) || /^\d+$/.test(value)
             },
-            /**
-             * 验证身份证号码
-             */
+            // 验证身份证号码
             idcard(value) {
-                return that.optional(value) || /^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X)$/.test(value)
+                return that.optional(value) || /^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[012]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X)$/.test(value)
             },
-            /**
-             * 验证两个输入框的内容是否相同
-             */
+            // 验证两个输入框的内容是否相同
             equalTo(value, param) {
                 return that.optional(value) || value === that.data[param]
             },
-            /**
-             * 验证是否包含某个值
-             */
+            // 验证是否包含某个值
             contains(value, param) {
                 return that.optional(value) || value.indexOf(param) >= 0
             },
-            /**
-             * 验证最小长度
-             */
+            // 验证最小长度
             minlength(value, param) {
                 return that.optional(value) || value.length >= param
             },
-            /**
-             * 验证最大长度
-             */
+            // 验证最大长度
             maxlength(value, param) {
                 return that.optional(value) || value.length <= param
             },
-            /**
-             * 验证一个长度范围[min, max]
-             */
+            // 验证一个长度范围[min, max]
             rangelength(value, param) {
                 return that.optional(value) || (value.length >= param[0] && value.length <= param[1])
             },
-            /**
-             * 验证最小值
-             */
+            // 验证最小值
             min(value, param) {
                 return that.optional(value) || value >= param
             },
-            /**
-             * 验证最大值
-             */
+            // 验证最大值
             max(value, param) {
                 return that.optional(value) || value <= param
             },
-            /**
-             * 验证一个值范围[min, max]
-             */
+            // 验证一个值范围[min, max]
             range(value, param) {
                 return that.optional(value) || (value >= param[0] && value <= param[1])
             },
         }
     }
 
-    /**
-     * 添加自定义验证方法
-     * @param {String} name 方法名
-     * @param {Function} method 函数体，接收两个参数(value, param)，value表示元素的值，param表示参数
-     * @param {String} message 提示信息
-     */
+    // 添加自定义验证方法
     addMethod(name, method, message) {
         this.methods[name] = method
         this.defaults.messages[name] = message !== undefined ? message : this.defaults.messages[name]
     }
 
-    /**
-     * 判断验证方法是否存在
-     */
+    // 判断验证方法是否存在
     isValidMethod(value) {
         let methods = []
         for (let method in this.methods) {
@@ -202,9 +146,7 @@ class WxValidate {
         return methods.indexOf(value) !== -1
     }
 
-    /**
-     * 格式化提示信息模板
-     */
+    // 格式化提示信息模板
     formatTpl(source, params) {
         const that = this
         if (arguments.length === 1) {
@@ -231,9 +173,7 @@ class WxValidate {
         return source
     }
 
-    /**
-     * 判断规则依赖是否存在
-     */
+    // 判断规则依赖是否存在
     depend(param) {
         switch (typeof param) {
             case 'boolean':
@@ -250,29 +190,19 @@ class WxValidate {
         return param
     }
 
-    /**
-     * 判断输入值是否为空
-     */
+    // 判断输入值是否为空
     optional(value) {
         return !this.methods.required(value) && 'dependency-mismatch'
     }
 
-    /**
-     * 获取自定义字段的提示信息
-     * @param {String} param 字段名
-     * @param {Object} rule 规则
-     */
+    // 获取自定义字段的提示信息
     customMessage(param, rule) {
         const params = this.messages[param]
         const isObject = typeof params === 'object'
         if (params && isObject) return params[rule.method]
     }
 
-    /**
-     * 获取某个指定字段的提示信息
-     * @param {String} param 字段名
-     * @param {Object} rule 规则
-     */
+    // 获取某个指定字段的提示信息
     defaultMessage(param, rule) {
         let message = this.customMessage(param, rule) || this.defaults.messages[rule.method]
         let type = typeof message
@@ -286,12 +216,7 @@ class WxValidate {
         return message
     }
 
-    /**
-     * 缓存错误信息
-     * @param {String} param 字段名
-     * @param {Object} rule 规则
-     * @param {String} value 元素的值
-     */
+    // 缓存错误信息
     formatTplAndAdd(param, rule, value) {
         let msg = this.defaultMessage(param, rule)
 
@@ -302,12 +227,7 @@ class WxValidate {
         })
     }
 
-    /**
-     * 验证某个指定字段的规则
-     * @param {String} param 字段名
-     * @param {Object} rules 规则
-     * @param {Object} data 需要验证的数据对象
-     */
+    // 验证某个指定字段的规则
     checkParam(param, rules, data) {
 
         // 缓存数据对象
@@ -347,10 +267,7 @@ class WxValidate {
         }
     }
 
-    /**
-     * 设置字段的默认验证值
-     * @param {String} param 字段名
-     */
+    // 设置字段的默认验证值
     setView(param) {
         this.form[param] = {
             $name: param,
@@ -362,13 +279,7 @@ class WxValidate {
         }
     }
 
-    /**
-     * 设置字段的验证值
-     * @param {String} param 字段名
-     * @param {String} method 字段的方法
-     * @param {Boolean} result 是否通过验证
-     * @param {String} value 字段的值
-     */
+    // 设置字段的验证值
     setValue(param, method, result, value) {
         const params = this.form[param]
         params.$valid = result
@@ -378,10 +289,7 @@ class WxValidate {
         params.$viewValue = value
     }
 
-    /**
-     * 验证所有字段的规则，返回验证是否通过
-     * @param {Object} data 需要验证数据对象
-     */
+    // 验证所有字段的规则，返回验证是否通过
     checkForm(data) {
         this.__initData()
 
@@ -393,23 +301,17 @@ class WxValidate {
         return this.valid()
     }
 
-    /**
-     * 返回验证是否通过
-     */
+    // 返回验证是否通过
     valid() {
         return this.size() === 0
     }
 
-    /**
-     * 返回错误信息的个数
-     */
+    // 返回错误信息的个数
     size() {
         return this.errorList.length
     }
 
-    /**
-     * 返回所有错误信息
-     */
+    // 返回所有错误信息
     validationErrors() {
         return this.errorList
     }
